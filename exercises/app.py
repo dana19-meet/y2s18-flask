2f964cf3499fd6522ee44549eb6a0308db9c4dd3
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:12:36 +0300

    app

--- exercises/app.py
+++ exercises/app.py
@@ -3,8 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
+    dancers = ["batseva", "ohad neharin", "maddie"]
     return render_template(
-    	"index.html")
+        "index.html" ,
+        dancers=dancers,
+        likes_same_sport=True)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True)
